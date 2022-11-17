09a6b1f2d215122b0f6534cac3633ad9aa6237d5
bubinq <emilbumov@gmail.com>
2022-11-17 12:44:37 +0200

    fixed index

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ import authRoutes from "./routes/auth.js";
 import goalRoutes from "./routes/goals.js";
 import toDoRoutes from "./routes/toDos.js";
 import progressRoutes from "./routes/progress.js";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 dotenv.config();
 
@@ -24,16 +28,16 @@ const establishConnection = () => {
 
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use("/api/goals", goalRoutes)
-app.use("/api/toDos", toDoRoutes)
-app.use("/api/progress", progressRoutes)
+app.use("/api/goals", goalRoutes);
+app.use("/api/toDos", toDoRoutes);
+app.use("/api/progress", progressRoutes);
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"))
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8000, () => {
